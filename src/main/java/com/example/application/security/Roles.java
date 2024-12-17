16efbcaeff74e605ca8663630b869a2212d71cb8
Jhonatas Coutinho <jhonatas.cjw@outlook.com>
2024-12-16 22:16:32 -0300

    save tutorial code

---
+++ src/main/java/com/example/application/security/Roles.java
@@ -0,0 +1,11 @@
+package com.example.application.security;
+
+public class Roles {
+
+    private Roles() {
+    }
+
+    public static final String USER = "USER";
+    public static final String ADMIN = "ADMIN";
+
+}
